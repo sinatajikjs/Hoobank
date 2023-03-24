b01f32637c037ec7d70b9955805749bddde869e9
Sina Tajik <102314451+SinaTajikJS@users.noreply.github.com>
2023-03-24 15:18:17 +0330

    Hero component created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
       <div
-        className={`grow ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className="flex flex-row items-center py-1.5 px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img className="w-8 h-8" src={discount} alt="discount" />
@@ -23,12 +23,42 @@ const Hero = () => {
           <h1 className="flex-1 text-white font-poppins ss:text-[72px] text-[52px] ss:leading-[100px] leading-[75px] font-semibold ">
             {" "}
             The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span> Payment Method.
+            <span className="text-gradient">Generation</span>
           </h1>
-          <div className="flex  md:mr-4 mr-0">
+          <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
+
+        <h1 className="text-white font-poppins ss:text-[68px] text-[52px] ss:leading-[100px] leading-[75px] font-semibold w-full">
+          Payment Method.
+        </h1>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
+      </div>
+
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
+        <img
+          src={robot}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
       </div>
     </section>
   );
