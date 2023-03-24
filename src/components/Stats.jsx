fbf4bf589e28ff5c1a922aab0d659cc6695be1d4
Sina Tajik <102314451+SinaTajikJS@users.noreply.github.com>
2023-03-24 15:52:14 +0330

    Stats component created

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { stats } from "../constants";
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <section className="flex justify-between flex-wrap items-center px-5 pr-28 gap-5 sm:mb-20 mb-6">
+      {stats.map((stat) => {
+        return (
+          <div key={stat.id} className="flex items-center gap-3">
+            <h1 className="text-white font-poppins font-semibold xs:text-[40px] xs:leading-[53.16px] leading-[43.16px] text-[30px]">
+              {stat.value}
+            </h1>
+            <p className="text-gradient font-poppins text-[20px] uppercase">
+              {stat.title}
+            </p>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Stats
+export default Stats;
