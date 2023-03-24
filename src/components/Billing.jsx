632094aaa7c1dd98b4f7ac9b5efd62a858b5ba3d
Sina Tajik <102314451+SinaTajikJS@users.noreply.github.com>
2023-03-24 22:08:57 +0330

    Billing component Created

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { apple, bill, google } from "@/assets";
 
 const Billing = () => {
   return (
-    <div>Billing</div>
-  )
-}
+    <section className="flex md:flex-row flex-col-reverse sm:py-16 py-6 gap-10">
+      <div className="flex-1 relative">
+        <img src={bill} alt="bill" className="w-full h-full relative z-[5]" />
+        {/* gradient start */}
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+        {/* gradient end */}
+      </div>
+      <div className="flex-1 flex flex-col gap-5 justify-center items-start ">
+        <h1 className="font-poppins text-white text-5xl leading-[76px] font-semibold w-full">
+          Easily control your <br /> billing & invoicing
+        </h1>
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30px] max-w-[470px]">
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="flex gap-5 mt-5">
+          <img src={apple} alt="app-store" />
+          <img src={google} alt="google-play" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Billing
+export default Billing;
